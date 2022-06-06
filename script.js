aa33b0b3c84cce3f03999a21504bb0174b3e46e7
nickgrief <wotniga@gmail.com>
2022-06-06 12:07:46 +0300

    Add basic operations

---
+++ script.js
@@ -0,0 +1,36 @@
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+
+    default:
+      console.log("Incorrect input");
+      break;
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
